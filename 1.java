840b6a40dcf73b26032b1c1f7ea2d2b3c1b441bd
wxr031 <wxr031@gmail.com>
2018-04-22 19:44:59 +0800

    remove arrayLength variable

--- 1.java
+++ 1.java
@@ -1,8 +1,7 @@
 class Solution {
 	public int[] twoSum(int[] nums, int target) {
 		Map<Integer,Integer> map = new HashMap<>();
-		int arrayLength = nums.length;
-		for(int i = 0; i < arrayLength; ++i) {
+		for(int i = 0; i < nums.length; ++i) {
 			if(map.containsKey(target - nums[i])) {
 				return new int[] {map.get(target - nums[i]), i};
 			} else {
