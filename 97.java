7462b5e9e27aaea3294b85d5d487c8e4d45b6723
wxr031 <wxr031@gmail.com>
2018-04-27 19:04:51 +0800

    add 97.java

---
+++ 97.java
@@ -0,0 +1,21 @@
+class Solution {
+	public boolean isInterleave(String s1, String s2, String s3) {
+		if(s1 == null || s2 == null || s3 == null || s1.length() + s2.length() != s3.length())
+			return false;
+		boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
+		dp[0][0] = true;
+		for(int i = 1; i <= s1.length() && s1.charAt(i-1) == s3.charAt(i-1); i++) {
+			dp[i][0] = true;
+		}
+		for(int i = 1; i <= s2.length() && s2.charAt(i-1) == s3.charAt(i-1); i++) {
+			dp[0][i] = true;
+		}
+		for(int i = 1; i <= s1.length(); i++) {
+			for(int j = 1; j <= s2.length(); j++) {
+				dp[i][j] = (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1))
+					|| (dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1));
+			}
+		}
+		return dp[s1.length()][s2.length()];
+	}
+}
