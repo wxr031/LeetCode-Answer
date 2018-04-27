3c026b3b5a602deb27cf3a253cab9b8fba4fda3c
wxr031 <wxr031@gmail.com>
2018-04-27 15:46:15 +0800

    add boundary checking

--- 60.java
+++ 60.java
@@ -1,12 +1,13 @@
 class Solution {
 	public String getPermutation(int n, int k) {
-		if(n == 0) return ""; 
+		if(n == 0 || k <= 0) return ""; 
 		int factorial = 1;
 		List<Integer> list = new LinkedList<>();
 		for(int i = 1; i <= n; i++) {
 			factorial *= i;
 			list.add(i);
 		}
+		if(k > factorial) return "";
 		k--; // make index start at 0
 		StringBuilder sb = new StringBuilder();
 		while(n > 0) {
