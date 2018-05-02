e344633668e30bc1369309c2f3111e6262e28f7e
wxr031 <wxr031@gmail.com>
2018-05-02 17:01:49 +0800

    using binary to grey code convertion

--- 89.java
+++ 89.java
@@ -2,9 +2,7 @@ class Solution {
 	public List<Integer> grayCode(int n) {
 		List<Integer> result = new ArrayList<>(Arrays.asList(0));
 		for(int i = 0; i < n; i++) {
-			for(int j = (1 << i) - 1; j >= 0; j--) {
-				result.add(result.get(j) | (1 << i));
-			}
+			result.add(i ^ (i>>1));
 		}
 		return result;
 	}
