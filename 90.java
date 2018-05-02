e69bbb66574ef95501db27501dd7b802ae89f3af
wxr031 <wxr031@gmail.com>
2018-05-02 12:53:04 +0800

    change to iterative solution

--- 90.java
+++ 90.java
@@ -1,15 +1,17 @@
 class Solution {
 	public List<List<Integer>> subsetsWithDup(int[] nums) {
-		List<List<Integer>> result = new ArrayList<>();
-		List<Integer> list = new ArrayList<>();
-		return subsetUtil(result, nums, pos);
-	}
-	public void subsetUtil(List<List<Integer>> result, List<Integer> temp, int[] nums, int pos) {
-		if(pos == nums.length) {
-			
+		List<List<Integer>> result = new ArrayList<List<Integer>>(Arrays.asList(Arrays.asList()));
+		Arrays.sort(nums);
+		int last = 0;
+		for(int i = 0; i < nums.length; i++) {
+			int begin = (i == 0 || (nums[i] == nums[i-1])) ? last : 0;
+			last = result.size();
+			for(int j = begin; j < last; j++) {
+				List<Integer> build = new ArrayList<Integer>(result.get(j));
+				build.add(nums[i]);
+				result.add(build);
+			}
 		}
-		result.add(nums[pos])
-		subsetUtil(result, nums, pos+1);
-
+		return result;
 	}
 }
