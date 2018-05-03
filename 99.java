8aaa32611b06b731213e6fb7893f0c7e1bf41d20
wxr031 <wxr031@gmail.com>
2018-05-03 17:08:06 +0800

    add 99.java

---
+++ 99.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	TreeNode change1 = null, change2 = null;
+	TreeNode prevNode = null;
+	public void recoverTree(TreeNode root) {
+		recoverTreeUtil(root);
+
+		int temp = change1.val;
+		change1.val = change2.val;
+		change2.val = temp;
+	}
+	public void recoverTreeUtil(TreeNode root) {
+		if(root == null) return;
+		recoverTreeUtil(root.left);
+		if(change1 == null && prevNode != null && prevNode.val > root.val) {
+			change1 = prevNode;
+		}
+		if(change1 != null && prevNode != null && prevNode.val > root.val) {
+			change2 = root;
+		}
+		prevNode = root;
+		recoverTreeUtil(root.right);
+	}
+}
