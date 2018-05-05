e98642d9a1d962cf20580a90b06d063d20d36c21
wxr031 <wxr031@gmail.com>
2018-05-05 20:45:10 +0800

    add 123.java

---
+++ 123.java
@@ -0,0 +1,13 @@
+class Solution {
+	public int maxProfit(int[] prices) {
+		int sell2 = 0, hold2 = Integer.MIN_VALUE, sell1 = 0, hold1 = Integer.MIN_VALUE;
+		for(int i = 0; i < prices.length; i++) {
+			sell2 = Math.max(sell2, hold2 + prices[i]);
+			hold2 = Math.max(hold2, sell1 - prices[i]);
+			sell1 = Math.max(sell1, hold1 + prices[i]);
+			hold1 = Math.max(hold1, -prices[i]);
+			//System.out.println(sell2 + " " + hold2 + " " + sell1 + " " + hold1);
+		}
+		return Math.max(sell1, sell2);
+	}
+}
