bfeb41d44f4e73941df15a3a63d159ea58dbb071
wxr031 <wxr031@gmail.com>
2018-05-06 14:05:57 +0800

    space optimization

--- 198.java
+++ 198.java
@@ -1,13 +1,12 @@
 class Solution {
 	public int rob(int[] nums) {
 		if(nums == null || nums.length == 0) return 0;
-		if(nums.length == 1) return nums[0];
-		int[] dp = new int[nums.length];
-		dp[0] = nums[0];
-		dp[1] = (nums[0] > nums[1]) ? nums[0] : nums[1];
-		for(int i = 2; i < nums.length; i++) {
-			dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+		int rob = 0, nrob = 0;
+		for(int num : nums) {
+			int robTemp = rob;
+			rob = Math.max(rob, nrob + num);
+			nrob = robTemp;
 		}
-		return dp[nums.length-1];
+		return Math.max(rob, nrob);
 	}
 }
