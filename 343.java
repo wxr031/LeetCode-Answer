dc6f160dace5bd05cc73f2733c2b9badaed8c1ca
wxr031 <wxr031@gmail.com>
2018-05-19 11:57:33 +0800

    add 343.java

--- 343.java
+++ 343.java
@@ -1,5 +1,6 @@
-class Solution {
+ass Solution {
 	public int integerBreak(int n) {
+		if(n < 4) return (n & 1) + (n >> 1);
 		if(n % 3 == 0) return power3(n / 3);
 		else if(n % 3 == 1) return 4 * power3((n / 3) - 1);
 		else return 2 * power3(n / 3);
@@ -10,3 +11,4 @@ class Solution {
 		return result;
 	}
 }
+
