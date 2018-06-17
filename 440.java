5a9008dad913541a2249e5ebd15cd6a9feb5cdeb
wxr031 <wxr031@gmail.com>
2018-06-17 12:00:14 +0800

    add 440.java

---
+++ 440.java
@@ -0,0 +1,27 @@
+class Solution {
+	public int findKthNumber(int num, int k) {
+		if(k > num) return -1;
+		int curr = 1, step = 1;
+		while(step != k) {
+			long between = numberBetween((long)(curr), (long)(curr + 1), num);
+			if(step + between > k) {
+				curr *= 10;
+				step++;
+			}
+			else {
+				curr++;
+				step += between;
+			}
+		}
+		return (int)curr;
+	}
+	private int numberBetween(long curr, long next, int num) {
+		int total = 0;
+		while(curr <= num) {
+			total += Math.min(next, num + 1) - curr;
+			curr *= 10;
+			next *= 10;
+		}
+		return total;
+	}
+}
