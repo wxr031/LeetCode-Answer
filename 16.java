542c3f1c719ef11a95ad7665c86ff104af3c7e3b
wxr031 <wxr031@protonmail.com>
2018-07-13 15:49:22 +0800

    add 16.java

---
+++ 16.java
@@ -0,0 +1,28 @@
+class Solution {
+	public int threeSumClosest(int[] nums, int target) {
+		assert nums.length >= 3;
+		Arrays.sort(nums);
+		int result = Integer.MAX_VALUE, minDiff = Integer.MAX_VALUE;
+		for(int i = 0; i < nums.length - 2; i++) {
+			int left = i + 1, right = nums.length - 1;
+			int search = target - nums[i];
+			while(left < right) {
+				int sum = nums[left] + nums[right], diff;
+				if(sum == search) {
+					return target;
+				}
+				if((diff = Math.abs(sum - search)) < minDiff) {
+					minDiff = diff;
+					result = nums[i] + sum;
+				}
+				if(sum > search) {
+					right--;
+				}
+				else {
+					left++;
+				}
+			}
+		}
+		return result;
+	}
+}
