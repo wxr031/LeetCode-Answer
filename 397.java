b8fd8ae04f4bfd05e1e3b76c535150ee759cad04
wxr031 <wxr031@protonmail.com>
2018-07-14 09:23:26 +0800

    add 397.java

---
+++ 397.java
@@ -0,0 +1,18 @@
+class Solution {
+	public int integerReplacement(int n) {
+		int count = 0;
+		while(n != 1) {
+			if((n & 1) == 0) {
+				n >>>= 1;
+			}
+			else if(n != 3 && ((n >>> 1) & 1) == 1) {
+				n++;
+			}
+			else {
+				n--;
+			}
+			count++;
+		}
+		return count;
+	}
+}
