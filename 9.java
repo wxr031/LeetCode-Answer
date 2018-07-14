b0366b9570628e78fb5b61060eca8c657bb4a8bb
wxr031 <wxr031@protonmail.com>
2018-07-14 20:46:18 +0800

    change private to public

--- 9.java
+++ 9.java
@@ -1,4 +1,8 @@
 class Solution {
+	public boolean isPalindrome(int x) {
+		if(x < 0) return false;
+		return x == reverse(x);
+	}
 	public int reverse(int x) {
 		int y = 0;
 		while(x != 0) {
@@ -6,9 +10,5 @@ class Solution {
 			x /= 10;
 		}
 		return y;
-	}
-	public boolean isPalindrome(int x) {
-		if(x < 0) return false;
-		return x == reverse(x);
 	}
 }
