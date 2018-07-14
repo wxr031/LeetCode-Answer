ce7453efcb1ece157f96e79ad1f30955f0e81c06
wxr031 <wxr031@protonmail.com>
2018-07-15 00:11:23 +0800

    add private keyword

--- 23.java
+++ 23.java
@@ -7,24 +7,25 @@
  * }
  */
 class Solution {
-	ListNode mergeTwoList(ListNode node1, ListNode node2) {
+	public ListNode mergeKLists(ListNode[] lists) {
+		if(list == null || lists.length == 0) return null;
+		return mergeKListUtil(lists, 0, lists.length-1);
+	}
+	private ListNode mergeKListUtil(ListNode[] lists, int left, int right) {
+		if(left == right) return lists[left];
+		int mid = left + (right-left) / 2;
+		return mergeTwoList(mergeKListUtil(lists, left, mid), mergeKListUtil(lists, mid+1 ,right));
+	}
+	private ListNode mergeTwoList(ListNode node1, ListNode node2) {
 		if(node1 == null) return node2;
 		if(node2 == null) return node1;
 		if(node1.val < node2.val) {
 			node1.next = mergeTwoList(node1.next, node2);
 			return node1;
-		} else {
+		}
+		else {
 			node2.next = mergeTwoList(node1, node2.next);
 			return node2;
 		}
-	}
-	ListNode mergeKListUtil(ListNode[] lists, int left, int right) {
-		if(left == right) return lists[left];
-		int mid = left + (right-left) / 2;
-		return mergeTwoList(mergeKListUtil(lists, left, mid), mergeKListUtil(lists, mid+1 ,right));
-	}
-	public ListNode mergeKLists(ListNode[] lists) {
-		if(list == null || lists.length == 0) return null;
-		return mergeKListUtil(lists, 0, lists.length-1);
 	}
 }
