76a1a4b8e9d0a6c7e00ea3f72504901bcb72a2f6
wxr031 <wxr031@protonmail.com>
2018-07-22 20:04:34 +0800

    add 70.java

---
+++ 70.java
@@ -0,0 +1,12 @@
+class Solution {
+	public int climbStairs(int n) {
+		if(n <= 2) return n;
+		int prev2 = 1, prev1 = 2, curr = 0;
+		for(int i = 2; i < n; i++){
+			curr = prev1 + prev2;
+			prev2 = prev1;
+			prev1 = curr;
+		}
+		return curr;
+	}
+}
