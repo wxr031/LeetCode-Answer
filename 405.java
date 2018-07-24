012284c23e922d81fb52242270448ee57781bb96
wxr031 <wxr031@protonmail.com>
2018-07-24 14:11:06 +0800

    add 405.java

---
+++ 405.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public String toHex(int num) {
+		if(num == 0) {
+			return "0";
+		}
+		char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+		StringBuilder sb = new StringBuilder();
+		while(num != 0){
+			sb.insert(0, hex[num & 15]);
+			num >>>= 4;
+		}
+		return sb.toString();
+	}
+}
