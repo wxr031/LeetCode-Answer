440121b6e9d7bab487e119a47acfd7c14683b97c
wxr031 <wxr031@protonmail.com>
2018-07-30 19:51:46 +0800

    add 15.java

--- 15.java
+++ 15.java
@@ -1,27 +1,28 @@
 class Solution {
-	public static List<List<Integer>> threeSum(int[] nums) {
+	public List<List<Integer>> threeSum(int[] nums) {
 		Arrays.sort(nums);
 		List<List<Integer>> result = new ArrayList<>();
-		int prev = Integer.MIN_VALUE;
-		for(int pos = 0; pos < nums.length-2; pos++) {
-			if(prev != nums[pos]) {
-				int target = -nums[pos];
-				int left = pos+1, right = nums.length-1;
+		for(int i = 0; i < nums.length; i++) {
+			if(i == 0 || nums[i] != nums[i - 1]) {
+				int left = i + 1, right = nums.length - 1;
 				while(left < right) {
-					if(nums[left] + nums[right] < target) {
-						left++;
-					}
-					else if(nums[left] + nums[right] > target) {
+					if(nums[left] + nums[right] > -nums[i]) {
 						right--;
+					}
+					else if(nums[left] + nums[right] < -nums[i]) {
+						left++;
 					}
 					else {
-						result.add(Arrays.asList(nums[pos], nums[left], nums[right]));
-						while(left < nums.length-1 && nums[left] == nums[left+1]) left++;
-						while(right > 0 && nums[right] == nums[right-1]) right--;
-						right--;
+						result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+						while(left < nums.length - 1 && nums[left] == nums[left + 1]) {
+							left++;
+						}
+						while(right > 0 && nums[right] == nums[right - 1]) {
+							right--;
+						}
 						left++;
+						right--;
 					}
-					prev = nums[pos];
 				}
 			}
 		}
