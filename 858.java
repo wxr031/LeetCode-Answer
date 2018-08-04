7dbd0bc1142095b1d205acb3182e510a30233df9
wxr031 <wxr031@protonmail.com>
2018-08-04 14:27:16 +0800

    change public to private

--- 858.java
+++ 858.java
@@ -5,7 +5,7 @@ class Solution {
 		q /= gcd;
 		return ((q & 1) == 0) ? 0 : ((p & 1) == 0) ? 2 : 1;
 	}
-	public int euler(int p, int q) {
+	private int euler(int p, int q) {
 		if(q == 0) return p;
 		return euler(q, p % q);
 	}
