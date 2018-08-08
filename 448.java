a1deba8d9d9610174efcae79ce478cba02b395d1
wxr031 <wxr031@protonmail.com>
2018-08-08 19:46:52 +0800

    add 448.java

---
+++ 448.java
@@ -0,0 +1,17 @@
+class Solution {
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> list = new ArrayList<>();
+		for(int i = 0; i < nums.length; i++) {
+			int abs = Math.abs(nums[i]) - 1;
+			if(nums[abs] > 0) {
+				nums[abs] = -nums[abs];
+			}
+		}
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] > 0) {
+				list.add(i + 1);
+			}
+		}
+		return list;
+	}
+}
