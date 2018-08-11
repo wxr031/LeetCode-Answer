4b74c5be2d47239aee4981a48d1dd62b1bd480d8
wxr031 <wxr031@protonmail.com>
2018-08-11 11:06:17 +0800

    add 456.java

---
+++ 456.java
@@ -0,0 +1,22 @@
+class Solution {
+	public boolean find132pattern(int[] nums) {
+		if(nums.length < 3) {
+			return false;
+		}
+		Stack<Integer> stack = new Stack<>();
+		int mid = Integer.MIN_VALUE;
+		for(int i = nums.length - 1; i >= 0; i--) {
+			if(nums[i] < mid) {
+				return true;
+			}
+			else {
+				while(!stack.isEmpty() && nums[i] > stack.peek()) {
+					mid = stack.pop();
+				}
+				stack.push(nums[i]);
+			}
+		}
+		return false;
+	}
+}
+
