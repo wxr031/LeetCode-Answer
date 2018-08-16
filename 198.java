55cf7a92198e1d256618ebc1900224a3721a2475
wxr031 <wxr031@protonmail.com>
2018-08-16 15:09:13 +0800

    add 198.java

--- 198.java
+++ 198.java
@@ -3,9 +3,9 @@ class Solution {
 		if(nums == null || nums.length == 0) return 0;
 		int rob = 0, nrob = 0;
 		for(int num : nums) {
-			int robTemp = rob;
-			rob = Math.max(rob, nrob + num);
-			nrob = robTemp;
+			int temp = nrob;
+			nrob = Math.max(nrob, rob);
+			rob = temp + num;
 		}
 		return Math.max(rob, nrob);
 	}
