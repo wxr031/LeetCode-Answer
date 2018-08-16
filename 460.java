72fd16db6011f5b6d4dcd1470807291329645be8
wxr031 <wxr031@protonmail.com>
2018-08-16 14:17:18 +0800

    add 460.java

--- 460.java
+++ 460.java
@@ -1,20 +1,57 @@
 class LFUCache {
 
-	PriorityQueue<HashMap<Integer, Integer>> pq;
-	int capacity;
+	int remain, minFreq;
+	HashMap<Integer, int[]> data; // [value, count]
+	HashMap<Integer, LinkedHashSet<Integer>> freq;
 
 	public LFUCache(int capacity) {
-		this.capacity = capacity;
-		pq = new PriorityQueue<>(capacity, (Map.Entry<Integer, Integer> entry1, Map.Entry<Integer, Integer> entry2)
-				-> {return entry1.getValue});
+		remain = (capacity > 0) ? capacity : -1;
+		minFreq = 0;
+		data = new HashMap<>();
+		freq = new HashMap<>();
 	}
 
 	public int get(int key) {
-
+		if(!data.containsKey(key) || remain == -1) {
+			return -1;
+		}
+		int count = data.get(key)[1];
+		data.get(key)[1] = count + 1;
+		freq.get(count).remove(key);
+		if(freq.get(minFreq).size() == 0) {
+			minFreq++;
+		}
+		if(!freq.containsKey(count + 1)) {
+			freq.put(count + 1, new LinkedHashSet<>());
+		}
+		freq.get(count + 1).add(key);
+		return data.get(key)[0];
 	}
 
 	public void put(int key, int value) {
-
+		if(remain == -1) {
+			return;
+		}
+		if(data.containsKey(key)) {
+			data.get(key)[0] = value;
+			get(key);
+			return;
+		}
+		if(remain == 0) {
+			int delete = freq.get(minFreq).iterator().next();
+			freq.get(minFreq).remove(delete);
+			if(freq.get(minFreq).size() == 0) {
+				freq.remove(minFreq);
+			}
+			data.remove(delete);
+		}
+		else {
+			remain--;
+		}
+		minFreq = 1;
+		data.put(key, new int[] {value, 1});
+		freq.putIfAbsent(1, new LinkedHashSet<>());
+		freq.get(1).add(key);
 	}
 }
 
