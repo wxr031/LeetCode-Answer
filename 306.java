bc040fb331694562d66b92fc2ad488877e9d0578
wxr031 <wxr031@protonmail.com>
2018-08-28 23:54:16 +0800

    adjust if...continue to the top of the loop

--- 306.java
+++ 306.java
@@ -4,10 +4,10 @@ class Solution {
 		int len = num.length();
 		for(int i = 1; i <= (len >> 1); i++) {
 			for(int j = i + 1; Math.max(i, j - i) <= len - j; j++) {
-				BigInteger num1 = new BigInteger(num.substring(0, i));
-				BigInteger num2 = new BigInteger(num.substring(i, j));
 				if((i > 1 && num.charAt(0) == '0') || (j - i > 1 && num.charAt(i) == '0'))
 					continue;
+				BigInteger num1 = new BigInteger(num.substring(0, i));
+				BigInteger num2 = new BigInteger(num.substring(i, j));
 				int end = j;
 				while(end < len) {
 					num2 = num2.add(num1);
