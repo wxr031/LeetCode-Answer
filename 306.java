8d59e5eb492d397ab3f5b5f8508ea620c287a2a8
wxr031 <wxr031@protonmail.com>
2018-08-28 23:53:02 +0800

    add 306.java

---
+++ 306.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+class Solution {
+	public boolean isAdditiveNumber(String num) {
+		int len = num.length();
+		for(int i = 1; i <= (len >> 1); i++) {
+			for(int j = i + 1; Math.max(i, j - i) <= len - j; j++) {
+				BigInteger num1 = new BigInteger(num.substring(0, i));
+				BigInteger num2 = new BigInteger(num.substring(i, j));
+				if((i > 1 && num.charAt(0) == '0') || (j - i > 1 && num.charAt(i) == '0'))
+					continue;
+				int end = j;
+				while(end < len) {
+					num2 = num2.add(num1);
+					num1 = num2.subtract(num1);
+					String next = num2.toString();
+					if(!num.startsWith(next, end)) break;
+					end += next.length();
+				}
+				if(end == len)
+					return true;
+			}
+		}
+		return false;
+	}
+}
