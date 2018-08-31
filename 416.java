2f88531317ea11928abd8bd9df899b55820751c0
wxr031 <wxr031@protonmail.com>
2018-08-31 14:53:57 +0800

    space optimization

--- 416.java
+++ 416.java
@@ -1,23 +1,16 @@
 class Solution {
 	public boolean canPartition(int[] nums) {
-		int target = 0;
-		for(int num : nums) {
-			target += num;
-		}
-		if((target & 1) == 1) {
-			return false;
-		}
-		target >>= 1;
-		boolean[][] dp = new boolean[nums.length + 1][target + 1];
-		dp[0][0] = true;
-		for(int i = 1; i <= nums.length; i++) {
-			for(int j = 0; j <= target; j++) {
-				dp[i][j] = dp[i - 1][j];
-				if(j >= nums[i - 1]) {
-					dp[i][j] |= dp[i - 1][j - nums[i - 1]];
-				}
+		int sum = 0;   
+		for (int num : nums) sum += num;
+		if ((sum & 1) == 1) return false;
+		sum >>= 1;
+		boolean[] dp = new boolean[sum + 1];
+		dp[0] = true;
+		for (int num : nums) {
+			for (int i = sum; i >= num; i--) {
+				dp[i] = dp[i] || dp[i - num];
 			}
 		}
-		return dp[nums.length][target];
+		return dp[sum];
 	}
 }
