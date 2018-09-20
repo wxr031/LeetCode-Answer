3eb07e72b3c02d18bde539c18a2be4bd6a5d945c
wxr031 <wxr031@protonmail.com>
2018-09-20 22:05:39 +0800

    add 312.java

--- 312.java
+++ 312.java
@@ -1,21 +1,18 @@
 class Solution {
 	public int maxCoins(int[] nums) {
-		List<Integer> list = new ArrayList<>();
-		list.add(1);
-		for(int num : nums) {
-			if(num != 0) list.add(num);
-		}
-		list.add(1);
-		int[][] dp = new int[list.size()][list.size()];
-		for(int gap = 2; gap < list.size(); gap++) {
-			for(int i = 0; i < list.size() - gap; i++) {
+		int[] balloons = new int[nums.length + 2];
+		balloons[0] = balloons[nums.length + 1] = 1;
+		System.arraycopy(nums, 0, balloons, 1, nums.length);
+		int[][] dp = new int[balloons.length][balloons.length];
+		for(int gap = 2; gap < balloons.length; gap++) {
+			for(int i = 0; i < balloons.length - gap; i++) {
 				int j = i + gap;
 				for(int k = i + 1; k < j; k++) {
-					dp[i][j] = Math.max(list.get(i) * list.get(j) * list.get(k)
+					dp[i][j] = Math.max(balloons[i] * balloons[j] * balloons[k]
 							+ dp[i][k] + dp[k][j], dp[i][j]);
 				}
 			}
 		}
-		return dp[0][list.size()-1];
+		return dp[0][balloons.length - 1];
 	}
 }
