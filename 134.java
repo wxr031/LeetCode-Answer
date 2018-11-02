ff09333b71accd1f761c7a504b322f4406e29eba
wxr031 <wxr031@protonmail.com>
2018-11-02 16:57:24 +0800

    add 134.java

--- 134.java
+++ 134.java
@@ -4,13 +4,13 @@ class Solution {
 			gas[i] -= cost[i];
 		}
 		int left = 0, right = 0, total = gas[0];
-		while(right + 1 != left && (left != 0 || right != gas.length-1)) {
+		while(right + 1 != left && (left != 0 || right != gas.length - 1)) {
 			if(total >= 0) {
-				right = (right == gas.length-1) ? 0 : right+1;
+				right = right == gas.length - 1 ? 0 : right+1;
 				total += gas[right];
 			}
 			else {
-				left = (left == 0) ? gas.length-1 : left-1;
+				left = left == 0 ? gas.length - 1 : left-1;
 				total += gas[left];
 			}
 		}
