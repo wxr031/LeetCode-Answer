f7d232ea3541e5198ab560d54ae705f3370fe6cb
wxr031 <wxr031@gmail.com>
2018-12-03 21:44:02 +0800

    add 560.java

---
+++ 560.java
@@ -0,0 +1,13 @@
+class Solution {
+	public int subarraySum(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+		int sum = 0, total = 0;
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			total += map.getOrDefault(sum - k, 0);
+			map.put(sum, map.getOrDefault(sum, 0) + 1);
+		}
+		return total;
+	}
+}
