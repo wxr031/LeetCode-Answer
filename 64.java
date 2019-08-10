fb0432185169b29dd7f044ec6e137b95604dd6fb
wxr031 <wxr031@gmail.com>
2019-08-10 21:42:33 +0800

    change variables and Arrays.fill

--- 64.java
+++ 64.java
@@ -1,21 +1,16 @@
 class Solution {
 	public int minPathSum(int[][] grid) {
-		if(grid == null || grid.length == 0 || grid[0].length == 0)
-			return 0;
-		int length = grid.length, width = grid[0].length;
-		int[][] dp = new int[length][width];
-		dp[0][0] = grid[0][0];
-		for(int i = 1; i < length; i++) {
-			dp[i][0] = dp[i-1][0] + grid[i][0];
-		} 
-		for(int i = 1; i < width; i++) {
-			dp[0][i] = dp[0][i-1] + grid[0][i];
-		} 
-		for(int i = 1; i < length; i++) {
-			for(int j = 1; j < width; j++) {
-				dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+		int row = grid.length;
+		int column = grid[0].length;
+		int[][] dp = new int[row + 1][column + 1];
+		Arrays.fill(dp[0], Integer.MAX_VALUE);
+		dp[0][1] = 0;
+		for (int i = 1; i <= row; i++) {
+			dp[i][0] = Integer.MAX_VALUE;
+			for (int j = 1; j <= column; j++) {
+				dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
 			}
 		}
-		return dp[length-1][width-1];
+		return dp[row][column];
 	}
 }
