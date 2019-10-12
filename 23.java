73b8a2a76bf1bdb50d7009590a007a3b328eda16
wxr031 <wxr031@gmail.com>
2019-10-12 10:06:16 +0800

    rewrite 23.java with Arrays.copyOfRange()

--- 23.java
+++ 23.java
@@ -1,31 +1,37 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *	 int val;
- *	 ListNode next;
- *	 ListNode(int x) { val = x; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
  * }
  */
 class Solution {
 	public ListNode mergeKLists(ListNode[] lists) {
-		if(list == null || lists.length == 0) return null;
-		return mergeKListUtil(lists, 0, lists.length - 1);
+		if (lists.length == 0) {
+			return null;
+		}
+		if (lists.length == 1) {
+			return lists[0];
+		}
+		return mergeTwoLists(
+				mergeKLists(Arrays.copyOfRange(lists, 0, lists.length / 2)),
+				mergeKLists(Arrays.copyOfRange(lists, lists.length / 2, lists.length)));
 	}
-	private ListNode mergeKListUtil(ListNode[] lists, int left, int right) {
-		if(left == right) return lists[left];
-		int mid = left + (right - left) / 2;
-		return mergeTwoList(mergeKListUtil(lists, left, mid), mergeKListUtil(lists, mid + 1 ,right));
-	}
-	private ListNode mergeTwoList(ListNode node1, ListNode node2) {
-		if(node1 == null) return node2;
-		if(node2 == null) return node1;
-		if(node1.val < node2.val) {
-			node1.next = mergeTwoList(node1.next, node2);
+	private ListNode mergeTwoLists(ListNode node0, ListNode node1) {
+		if (node0 == null) {
 			return node1;
 		}
+		if (node1 == null) {
+			return node0;
+		}
+		if (node0.val < node1.val) {
+			node0.next = mergeTwoLists(node0.next, node1);
+			return node0;
+		}
 		else {
-			node2.next = mergeTwoList(node1, node2.next);
-			return node2;
+			node1.next = mergeTwoLists(node0, node1.next);
+			return node1;
 		}
 	}
 }
