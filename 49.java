05199dbfe25d1cfdf17965054615321a3b5cd335
wxr031 <wxr031@gmail.com>
2019-11-09 15:38:32 +0800

    change solution: complexity turns from O(NLlogL) to O(NL)

--- 49.java
+++ 49.java
@@ -1,17 +1,24 @@
 class Solution {
 	public List<List<String>> groupAnagrams(String[] strs) {
-		List<List<String>> result = new ArrayList<>();
-		Map<String,Integer> map = new HashMap<>();
-		int indexCount = 0;
-		for(int i = 0; i < strs.length; i++) {
-			char[] temp = strs[i].toCharArray();
-			Arrays.sort(temp);
-			String key = String.valueOf(temp);
-			if(!map.containsKey(key)) {
-				map.put(key, indexCount++);
-				result.add(new ArrayList<String>());
+		Map<String, List<String>> map = new HashMap<>();
+		for (String str : strs) {
+			int[] counter = new int[256];
+			for (char c : str.toCharArray()) {
+				counter[c]++;
 			}
-			result.get(map.get(key)).add(strs[i]);
+			StringBuilder sb = new StringBuilder();
+			for (int n = 'a'; n <= 'z'; n++) {
+				for (int i = 0; i < counter[n]; i++) {
+					sb.append(n);
+				}
+			}
+			String sorted = sb.toString();
+			map.putIfAbsent(sorted, new ArrayList<>());
+			map.get(sorted).add(str);
+		}
+		List<List<String>> result = new ArrayList<>();
+		for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+			result.add(entry.getValue());
 		}
 		return result;
 	}
