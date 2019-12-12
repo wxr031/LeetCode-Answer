e203517b732b2687b87129caf6a114b0b6464503
wxr031 <wxr031@gmail.com>
2019-12-12 21:51:25 +0800

    question modified

--- 56.java
+++ 56.java
@@ -1,34 +1,27 @@
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 class Solution {
-	public List<Interval> merge(List<Interval> intervals) {
-		List<Interval> result = new ArrayList<>();
-		if(intervals == null || intervals.size() == 0) return result;
-		Collections.sort(intervals, new Comparator<Interval>() {
-			public int compare(Interval inter1, Interval inter2) {
-				return inter1.start - inter2.start;
+	public int[][] merge(int[][] intervals) {
+		if (intervals == null || intervals.length == 0) {
+			return new int[0][0];
+		}
+		Arrays.sort(intervals, (interval1, interval2) -> {
+			if (interval1[0] == interval2[0]) {
+				return interval1[1] - interval2[1];
 			}
+			return interval1[0] - interval2[0];
 		});
-        int prevStart = intervals.get(0).start, prevEnd = intervals.get(0).end;
-		for(int i = 1; i < intervals.size(); i++) {
-			if(intervals.get(i).start > prevEnd) {
-				result.add(new Interval(prevStart, prevEnd));
-				prevStart = intervals.get(i).start;
-				prevEnd = intervals.get(i).end;
+		List<int[]> list = new ArrayList<>();
+		int[] prevInterval = intervals[0];
+		for (int[] currInterval : intervals) {
+			if (currInterval[0] > prevInterval[1]) {
+				list.add(prevInterval);
+				prevInterval = currInterval;
 			}
-            else if(intervals.get(i).end > prevEnd) {
-				prevEnd = intervals.get(i).end;
+			else if (currInterval[1] >= prevInterval[1]) {
+				prevInterval[1] = currInterval[1];
 			}
 		}
-		result.add(new Interval(prevStart, prevEnd));
-		return result;
+		list.add(prevInterval);
+		int[][] result = new int[list.size()][2];
+		return list.toArray(result);
 	}
 }
-
